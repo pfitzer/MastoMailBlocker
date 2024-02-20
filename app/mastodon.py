139163311a4f1a2c3c7a8656e20780d77249361e
Michael Pfister <pfister@contria.com>
2024-02-20 16:27:44 +0100

    Refactor Mastodon class and improve payload building
    The Mastodon class has been significantly refactored to improve code readability and maintenance. Payload building code has been extracted to a separate method, allowing for more concise and readable method definitions. Furthermore, request sending logic has been abstracted into a separate method to reduce code duplication and improve error handling.

--- app/mastodon.py
+++ app/mastodon.py
@@ -1,95 +1,52 @@
-import urllib.parse
+from urllib.parse import urlencode
 
 import requests
-from django_q.tasks import async_task
-
-from app.exception import VerifyFailedException
 
 APP_NAME = "MastoMailBlocker"
 SCOPES = 'read admin:write:email_domain_blocks'
 
 
 class Mastodon:
-    """
-    Class Mastodon
-
-    A class for interacting with the Mastodon API.
-
-    Attributes:
-        client (Client): An instance of the Client class.
-
-    Methods:
-        __init__(client: Client = None):
-            Initializes a Mastodon instance with an optional client.
-
-        register_app():
-            Registers the application with the Mastodon API and returns the client ID and secret.
-
-        get_authorization_url(request, client_key: str):
-            Generates the authorization URL for the Mastodon API based on the provided request and client key.
-
-        obtain_access_token(code: str):
-            Obtains an access token from the Mastodon API using the provided authorization code.
-
-    """
-
     def __init__(self, client=None):
         self.client = client
 
     def register_app(self, request):
-        payload = {
-            'client_name': APP_NAME,
-            'redirect_uris': f'{request.scheme}://{request.META["HTTP_HOST"]}/get_code/{self.client.pk}/',
-            'scopes': SCOPES
-        }
-        req = requests.post(f'{self.client.client_url}/api/v1/apps', data=payload)
-        if req.status_code == 200:
-            response = req.json()
-            client_id = response['client_id']
-            client_secret = response['client_secret']
-            return client_id, client_secret
-        else:
-            return None, None
+        payload = self.build_payload('client_name', APP_NAME,
+                                     'redirect_uris', self.get_redirect_uri(request),
+                                     'scopes', SCOPES)
+
+        return self.send_request('post', 'api/v1/apps', payload)
 
     def get_authorization_url(self, request):
-        payload = {
-            'client_id': self.client.client_key,
-            'response_type': 'code',
-            'grant_type': 'authorization_code',
-            'redirect_uri': f'{request.scheme}://{request.META["HTTP_HOST"]}/get_code/{self.client.pk}/',
-            'scope': SCOPES
-        }
-        url = f'{self.client.client_url}/oauth/authorize/?'
-        return url + urllib.parse.urlencode(payload)
+        payload = self.build_payload('client_id', self.client.client_key,
+                                     'response_type', 'code',
+                                     'grant_type', 'authorization_code',
+                                     'redirect_uri', self.get_redirect_uri(request),
+                                     'scope', SCOPES)
 
-    def obtain_access_token(self, code: str, request):
-        payload = {
-            'grant_type': 'authorization_code',
-            'code': code,
-            'client_id': self.client.client_key,
-            'client_secret': self.client.client_secret,
-            'redirect_uri': f'{request.scheme}://{request.META["HTTP_HOST"]}/get_code/{self.client.pk}/',
-            'scope': SCOPES
-        }
+        return self.get_client_url('oauth/authorize') + '?' + urlencode(payload)
 
-        req = requests.post(f'{self.client.client_url}/oauth/token', data=payload)
-        response = req.json()
-        return response['access_token']
+    def obtain_access_token(self, code, request):
+        payload = self.build_payload('grant_type', 'authorization_code',
+                                     'code', code,
+                                     'client_id', self.client.client_key,
+                                     'client_secret', self.client.client_secret,
+                                     'redirect_uri', self.get_redirect_uri(request),
+                                     'scope', SCOPES)
 
-    def send_domain_block(self, domain: str):
-        if not self.verify_credentials():
-            self.client.delete()
-            raise VerifyFailedException('Invalid credentials')
-        headers = {'Authorization': 'Bearer ' + self.client.access_token}
-        payload = {'domain': domain}
-        r = requests.post(f"{self.client.client_url}/api/v1/admin/email_domain_blocks", headers=headers,
-                          data=payload)
-        return r.status_code
+        return self.send_request('post', 'oauth/token', payload)['access_token']
 
-    def verify_credentials(self):
-        headers = {'Authorization': 'Bearer ' + self.client.access_token}
-        r = requests.get(f'{self.client.client_url}/api/v1/apps/verify_credentials', headers=headers)
-        return r.status_code == 200
+    def get_redirect_uri(self, request):
+        return f'{request.scheme}://{request.META["HTTP_HOST"]}/get_code/{self.client.pk}/'
 
-    def auth_ready(self):
-        async_task("app.tasks.initial_mail_adding", self)
+    def build_payload(self, *args):
+        return {args[i]: args[i + 1] for i in range(0, len(args), 2)}
+
+    def get_client_url(self, endpoint=''):
+        return f'{self.client.client_url}/{endpoint}'
+
+    def send_request(self, method, endpoint, payload=None, headers=None):
+        response = getattr(requests, method)(self.get_client_url(endpoint), data=payload, headers=headers)
+        if response.status_code != 200:
+            response.raise_for_status()
+        return response.json()
