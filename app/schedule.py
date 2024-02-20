0fe8d05953ed42ef82aa67d029fd304a6b51c8dc
Michael Pfister <pfister@contria.com>
2024-02-20 14:19:36 +0100

    Add cache table in Docker setup and refactor mail domain update
    Added a new function 'createcachetable' in the docker-compose file to create a cache table during setup. In the ‘schedule.py’ file, removed an unused 'time' import and refactored the 'update_mail_domains' method. The refactoring ensures domains are saved once, preventing potential IntegrityErrors, before sending block requests for each client.

--- app/schedule.py
+++ app/schedule.py
@@ -1,4 +1,3 @@
-import time
 import urllib.request
 
 from django.conf import settings
@@ -11,16 +10,13 @@ from app.models import Client, Domain
 def update_mail_domains():
     domains = urllib.request.urlopen(settings.DISPOSABLE_MAILS_URL)
     clients = Client.objects.all()
-    for client in clients:
-        mastodon = Mastodon(client)
-        for line in domains.readlines():
-            try:
-                domain = Domain()
-                domain.name = line.decode('utf-8').strip()
-                domain.save()
-                status_code = mastodon.send_domain_block(domain.name)
-                time.sleep(1)
-            except Exception:
-                continue
-            except IntegrityError:
-                continue
+    for line in domains.readlines():
+        try:
+            domain = Domain();
+            domain.name = line.decode('utf-8').strip()
+            domain.save()
+        except IntegrityError:
+            continue
+        for client in clients:
+            mastodon = Mastodon(client)
+            mastodon.send_domain_block(domain.name)
